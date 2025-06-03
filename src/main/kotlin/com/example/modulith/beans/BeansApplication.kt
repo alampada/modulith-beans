b1b5e8212dc1ad796d8858a5a8a439324fc2562d
Antonios Lampadaridis <antonios.lampadaridis@gmail.com>
2025-06-03 23:11:30 +0100

    Push missing files

---
+++ src/main/kotlin/com/example/modulith/beans/BeansApplication.kt
@@ -0,0 +1,11 @@
+package com.example.modulith.beans
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeansApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeansApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/modulith/beans/BeansApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.modulith.beans
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BeansApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
